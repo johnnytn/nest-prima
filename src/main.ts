27b010fc910a9be2b18d81b0afab769a5935ddf0
johnnytn <johnnytn@gmail.com>
2023-02-01 09:44:25 -0300

    feat: add prisma error handling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   if (process.env.NODE_ENV !== 'production') {
+    // Setup swagger module
     const options = new DocumentBuilder()
       .setTitle('API')
       .setDescription('The API documentation')
@@ -14,6 +16,10 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     const swaggerPath = '/docs';
     SwaggerModule.setup(swaggerPath, app, document);
+
+    // Apply prisma error handling
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   }
 
   await app.listen(3000);
